fa8cc6a21dcb5368f621882836629e1f465f5f7c
Carl Olson <colson10@gmail.com>
2018-03-04 21:24:35 -0800

    comments added

---
+++ carl-olson/server/server.js
@@ -0,0 +1,135 @@
+'use strict'
+
+// Application dependencies
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const superagent = require('superagent');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+const TOKEN = process.env.TOKEN;
+
+// COMMENT: Explain the following line of code. What is the API_KEY? Where did it come from?
+// Carl -- This is declaring a variable and assigning it the value of the api key we have for the google books api. The key was requested by the creator of the app and given by google. The actual value is either in our process environment variables or in the config vars on heroku/similar.
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+// Application Middleware
+app.use(cors());
+
+// API Endpoints
+app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
+
+app.get('/api/v1/books/find', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+
+  // COMMENT: Explain the following four lines of code. How is the query built out? What information will be used to create the query?
+  // Carl -- This is building the query string that will be added to the end of the url. It's using the values from the inputs in the search form. It's structured with conditionals so that it still works if the inputs didn't include values for each of them.
+  let query = ''
+  if(req.query.title) query += `+intitle:${req.query.title}`;
+  if(req.query.author) query += `+inauthor:${req.query.author}`;
+  if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
+
+  // COMMENT: What is superagent? How is it being used here? What other libraries are available that could be used for the same purpose?
+  // Carl -- Superagent is handling the request query to and response from the external api. There are probably many different libraries that do something similar...superagent is just a good and trustworthy one.
+  superagent.get(url)
+    .query({'q': query})
+    .query({'key': API_KEY})
+    .then(response => response.body.items.map((book, idx) => {
+
+      // COMMENT: The line below is an example of destructuring. Explain destructuring in your own words.
+      // Carl -- This is short hand version of declaring an object with certain properties from the response.
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+
+      // COMMENT: What is the purpose of the following placeholder image?
+      // Carl -- This will be used as the image for a book that does not have a thumbnail image in the response. 
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+      // COMMENT: Explain how ternary operators are being used below.
+      // Carl -- These ternary operators are handling situations where the response may not include a value for all the properties we're looking for.
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+        book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
+      }
+    }))
+    .then(arr => res.send(arr))
+    .catch(console.error)
+})
+
+// COMMENT: How does this route differ from the route above? What does ':isbn' refer to in the code below?
+// It's similar in that it's making a request to the api. It's different in that it's making a request based on a specific value, being the isbn, which was captured from the data-bookid in the handlebars template. This is returning an object of the book properties we need to ultimately post it to our db.
+app.get('/api/v1/books/find/:isbn', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  superagent.get(url)
+    .query({ 'q': `+isbn:${req.params.isbn}`})
+    .query({ 'key': API_KEY })
+    .then(response => response.body.items.map((book, idx) => {
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+      }
+    }))
+    .then(book => res.send(book[0]))
+    .catch(console.error)
+})
+
+app.get('/api/v1/books', (req, res) => {
+  client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE book_id=${req.params.id}`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(`
+    INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
+    [title, author, isbn, image_url, description]
+  )
+  .then(results => res.sendStatus(201))
+  .catch(console.error);
+});
+
+app.put('/api/v1/books/:id', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(`
+    UPDATE books
+    SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+    WHERE book_id=$6`,
+    [title, author, isbn, image_url, description, req.params.id]
+  )
+  .then(() => res.sendStatus(204))
+  .catch(console.error)
+})
+
+app.delete('/api/v1/books/:id', (req, res) => {
+  client.query('DELETE FROM books WHERE book_id=$1', [req.params.id])
+  .then(() => res.sendStatus(204))
+  .catch(console.error);
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
